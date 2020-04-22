ca519f35045b62e935bed60cdd830b785b474412
Kmerck88 <39713216+Kmerck88@users.noreply.github.com>
2020-04-22 08:49:38 -0400

    Feat: user can input Grade and added unit tests

---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Scott's Grade Book");
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+
+            }
+
+            var stats = book.GetStatistics();
+
+ 
+
+            Console.WriteLine(Book.CATEGORY);
+            Console.WriteLine($"For book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+    }
+}
---
+++ src/Gradebook/Statistics.cs
@@ -0,0 +1,12 @@
+namespace GradeBook 
+{ 
+    public class statistics 
+    {
+        public double Average; 
+        
+        public double High; 
+        public double Low; 
+        public char Letter; 
+
+    }
+}
